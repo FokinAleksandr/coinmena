65681325df0dc6889caf36696bfb65c024d6f26a
Alexander Fokin <shwayze@yandex.ru>
2022-07-04 10:27:46 +0300

    feat: filtering on countries list screen

--- src/screens/CountriesListScreen.tsx
+++ src/screens/CountriesListScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, TextInput } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import { useCovidCasesSummary } from '~/src/api/useCovidCasesSummary';
 import { colors, setOpacity } from '~/src/ui/colors';
@@ -10,9 +11,23 @@ import { Typography } from '~/src/ui/Typography';
 export function CountriesListScreen() {
   const { data, isLoading } = useCovidCasesSummary();
 
+  const [isOpen, setIsOpen] = React.useState(false);
   const [filterText, setFilterText] = React.useState('');
+  const [sortBy, setSortBy] = React.useState<'confirmed' | 'deaths' | 'recovered'>('confirmed');
 
   if (data) {
+    const filteredData = data.Countries.sort((a, b) => {
+      if (sortBy === 'deaths') {
+        return b.TotalDeaths - a.TotalDeaths;
+      } else if (sortBy === 'recovered') {
+        return b.TotalRecovered - a.TotalRecovered;
+      } else {
+        return b.TotalConfirmed - a.TotalConfirmed;
+      }
+    }).filter(country =>
+      country.Country.toLocaleLowerCase().includes(filterText.toLocaleLowerCase())
+    );
+
     return (
       <Column flex={1} margin={24}>
         <TextInput
@@ -22,13 +37,23 @@ export function CountriesListScreen() {
           value={filterText}
           onChangeText={setFilterText}
         />
+        <Typography variant="h3">sort by:</Typography>
+        <DropDownPicker
+          open={isOpen}
+          value={sortBy}
+          items={[
+            { label: 'confirmed', value: 'confirmed' },
+            { label: 'deaths', value: 'deaths' },
+            { label: 'recovered', value: 'recovered' },
+          ]}
+          setOpen={setIsOpen}
+          setValue={setSortBy}
+        />
         <Box marginTop={12}>
           <FlatList
-            data={data.Countries.filter(country =>
-              country.Country.toLocaleLowerCase().includes(filterText.toLocaleLowerCase())
-            )}
+            data={filteredData}
             renderItem={({ item }) => (
-              <Column key={item.ID}>
+              <Column key={item.ID} marginBottom={8}>
                 <Typography variant="h1">{item.Country}</Typography>
                 <CovidData
                   confirmed={item.TotalConfirmed}
@@ -58,10 +83,10 @@ const styles = StyleSheet.create({
   input: {
     paddingHorizontal: 8,
     height: 40,
-    width: 200,
     borderRadius: 6,
     backgroundColor: setOpacity(colors.electric, 0.2),
     marginRight: 12,
+    marginBottom: 12,
     color: colors.white,
   },
 });
